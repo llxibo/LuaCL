5a840d20295956286fad05b8922c00f1f151ff98
llxibo <llxibo@126.com>
2014-07-26 23:47:09 +0800

    Initialized luacl_buffer.hpp

---
+++ src/luacl_buffer.hpp
@@ -0,0 +1,7 @@
+#ifndef __LUACL_BUFFER_HPP
+#define __LUACL_BUFFER_HPP
+
+#include "LuaCL.h"
+
+
+#endif /* __LUACL_BUFFER_HPP */
--- src/luacl_cmdqueue.hpp
+++ src/luacl_cmdqueue.hpp
@@ -1,6 +1,7 @@
 #ifndef __LUACL_CMDQUEUE_HPP
 #define __LUACL_CMDQUEUE_HPP
 
+#include "LuaCL.h"
 
 
 #endif /* __LUACL_CMDQUEUE_HPP */
