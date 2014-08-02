0179f529e3fff80bbb1a0f37735fc7ab9dcfb83c
Aean <v@aean.net>
2014-08-03 01:42:52 +0800

    Code consistency.

--- src/luacl_endianness.hpp
+++ src/luacl_endianness.hpp
@@ -3,62 +3,60 @@
 
 #include <stdint.h>
 
-namespace util{
-	enum luacl_byte_order_enumeration {
-		LUACL_UNTESTED_ENDIAN,
-		LUACL_LITTLE_ENDIAN,
-		LUACL_BIG_ENDIAN,
-		LUACL_MIXED_ENDIAN,
-	};
-	
-	inline luacl_byte_order_enumeration luacl_host_byte_order(void) {
+namespace util {
+enum luacl_byte_order_enumeration {
+    LUACL_UNTESTED_ENDIAN,
+    LUACL_LITTLE_ENDIAN,
+    LUACL_BIG_ENDIAN,
+    LUACL_MIXED_ENDIAN,
+};
 
-		static luacl_byte_order_enumeration byte_order = LUACL_UNTESTED_ENDIAN;
-		if (byte_order != LUACL_UNTESTED_ENDIAN) {
-			return byte_order;
-		}
+inline luacl_byte_order_enumeration luacl_host_byte_order(void) {
 
-		char dest[sizeof(uint32_t)];
-		uint32_t src = UINT32_C( 0x01020304 );
-		/*
-			According to ISO C, to reinterpret bits as another data type,
-			the only strict correct method is to use `memcpy`.
-		*/
-		memcpy(dest, &src, sizeof(uint32_t));
-		
-		bool asc = true;
-		bool desc = true;
-		for (size_t i = 1; i < sizeof(uint32_t); i++) {
-			asc = asc && dest[i - 1] < dest[i];
-			desc = desc && dest[i - 1] > dest[i];
-		}
-		
-		if (!asc && desc) {
-			return byte_order = LUACL_LITTLE_ENDIAN;
-		}
-		else if (asc && !desc) {
-			return byte_order = LUACL_BIG_ENDIAN;
-		}
-		else {
-			return byte_order = LUACL_MIXED_ENDIAN;
-		}
-	}
+    static luacl_byte_order_enumeration byte_order = LUACL_UNTESTED_ENDIAN;
+    if (byte_order != LUACL_UNTESTED_ENDIAN) {
+        return byte_order;
+    }
 
-	template <typename T>
-	void luacl_byte_order_reverse(T * buffer, size_t size) {
-        char * src = reinterpret_cast<char *>(buffer + 1);
-		char wordBuffer[sizeof(T)];
+    char dest[sizeof(uint32_t)];
+    uint32_t src = UINT32_C( 0x01020304 );
+    /*
+    	According to ISO C, to reinterpret bits as another data type,
+    	the only strict correct method is to use `memcpy`.
+    */
+    memcpy(dest, &src, sizeof(uint32_t));
 
-		while (size--) {
-			for (size_t i = 0; i < sizeof(T); i++) {
-				memcpy(wordBuffer + i, --src, 1);
-			}
-			memcpy(buffer, wordBuffer, sizeof(T));
+    bool asc = true;
+    bool desc = true;
+    for (size_t i = 1; i < sizeof(uint32_t); i++) {
+        asc = asc && dest[i - 1] < dest[i];
+        desc = desc && dest[i - 1] > dest[i];
+    }
 
-			src += sizeof(T) * 2;
-			buffer++;
-		}
-	}
+    if (!asc && desc) {
+        return byte_order = LUACL_LITTLE_ENDIAN;
+    } else if (asc && !desc) {
+        return byte_order = LUACL_BIG_ENDIAN;
+    } else {
+        return byte_order = LUACL_MIXED_ENDIAN;
+    }
+}
+
+template <typename T>
+void luacl_byte_order_reverse(T * buffer, size_t size) {
+    char * src = reinterpret_cast<char *>(buffer + 1);
+    char wordBuffer[sizeof(T)];
+
+    while (size--) {
+        for (size_t i = 0; i < sizeof(T); i++) {
+            memcpy(wordBuffer + i, --src, 1);
+        }
+        memcpy(buffer, wordBuffer, sizeof(T));
+
+        src += sizeof(T) * 2;
+        buffer++;
+    }
+}
 }
 
 #endif /* __LUACL_ENDIANNESS_HPP */
--- src/luacl_math.hpp
+++ src/luacl_math.hpp
@@ -4,132 +4,116 @@
 #include <cmath>
 #include <vector>
 
-namespace util{
+namespace util {
 #define WOULD_BE_MODIFIED
 #define ARG_IN
 #define ARG_OUT
 
-	template <typename T>
-	T luacl_prefix_sum(ARG_IN WOULD_BE_MODIFIED std::vector<T>& data) {
-		for (size_t k = 1; k < data.size(); k += k) {
-			for (size_t i = 0; i < data.size(); i += k) {
-				auto j = i + k;
-				if (j < data.size()) {
-					data[i] += data[j];
-					i = j;
-				}
-				else {
-					break;
-				}
-			}
-		}
-		return data[0];
-	}
+template <typename T>
+T luacl_prefix_sum(ARG_IN WOULD_BE_MODIFIED std::vector<T>& data) {
+    for (size_t k = 1; k < data.size(); k += k) {
+        for (size_t i = 0; i < data.size(); i += k) {
+            auto j = i + k;
+            if (j < data.size()) {
+                data[i] += data[j];
+                i = j;
+            } else {
+                break;
+            }
+        }
+    }
+    return data[0];
+}
 
-	template <typename T>
-	void luacl_mean_and_stddev(ARG_IN T* _data, ARG_IN size_t n, ARG_OUT T& mean, ARG_OUT T& stddev) {
-		/* Make a copy of _data. */
-		std::vector<T> data(_data, _data + n);
+template <typename T>
+void luacl_mean_and_stddev(ARG_IN const T* _data, ARG_IN size_t n, ARG_OUT T& mean, ARG_OUT T& stddev) {
+    /* Make a copy of _data. */
+    std::vector<T> data(_data, _data + n);
 
-		/* Prefix-sum to minimize rounding errors for FP types. */
-		mean = luacl_prefix_sum(data) / static_cast<T>(n);
+    /* Prefix-sum to minimize rounding errors for FP types. */
+    mean = luacl_prefix_sum(data) / static_cast<T>(n);
 
-		/* data --> sqr(data - mean) */
-		for (size_t i = 0; i < data.size(); ++i) {
-			data[i] = _data[i] - mean;
-			data[i] *= data[i];
-		}
+    /* data --> sqr(data - mean) */
+    for (size_t i = 0; i < data.size(); ++i) {
+        data[i] = _data[i] - mean;
+        data[i] *= data[i];
+    }
 
-		/* sqr(data - mean) --> stddev */
-		stddev = sqrt(luacl_prefix_sum(data) / static_cast<T>(n));
-	}
-	
-	/* Phi(x) - the cumulative distribution function for standard normal distribution( mean = 0, stddev = 1 ). */
-	inline double luacl_stdnorm_cdf(double x) {
-		return 0.5 * (1 + erf(x * (1 / 1.41421356237309504880)));
-	}
+    /* sqr(data - mean) --> stddev */
+    stddev = sqrt(luacl_prefix_sum(data) / static_cast<T>(n));
+}
 
-	/* Phi^-1(clvl) - the inverse function of `lualc_stdnorm_cdf`. http://home.online.no/~pjacklam/notes/invnorm/ */
-	inline double luacl_stdnorm_cdf_inv(double clvl) {
-		/* Coefficients in rational approximations. */
-		static const double a[] =
-		{
-			-3.969683028665376e+01,
-			2.209460984245205e+02,
-			-2.759285104469687e+02,
-			1.383577518672690e+02,
-			-3.066479806614716e+01,
-			2.506628277459239e+00
-		};
+/* Phi(x) - the cumulative distribution function for standard normal distribution( mean = 0, stddev = 1 ). */
+inline double luacl_stdnorm_cdf(double x) {
+    return 0.5 * (1 + erf(x * (1 / 1.41421356237309504880)));
+}
 
-		static const double b[] =
-		{
-			-5.447609879822406e+01,
-			1.615858368580409e+02,
-			-1.556989798598866e+02,
-			6.680131188771972e+01,
-			-1.328068155288572e+01
-		};
+/* Phi^-1(clvl) - the inverse function of `lualc_stdnorm_cdf`. http://home.online.no/~pjacklam/notes/invnorm/ */
+inline double luacl_stdnorm_cdf_inv(double clvl) {
+    /* Coefficients in rational approximations. */
+    static const double a[] = {
+        -3.969683028665376e+01,
+        2.209460984245205e+02,
+        -2.759285104469687e+02,
+        1.383577518672690e+02,
+        -3.066479806614716e+01,
+        2.506628277459239e+00
+    };
 
-		static const double c[] =
-		{
-			-7.784894002430293e-03,
-			-3.223964580411365e-01,
-			-2.400758277161838e+00,
-			-2.549732539343734e+00,
-			4.374664141464968e+00,
-			2.938163982698783e+00
-		};
+    static const double b[] = {
+        -5.447609879822406e+01,
+        1.615858368580409e+02,
+        -1.556989798598866e+02,
+        6.680131188771972e+01,
+        -1.328068155288572e+01
+    };
 
-		static const double d[] =
-		{
-			7.784695709041462e-03,
-			3.224671290700398e-01,
-			2.445134137142996e+00,
-			3.754408661907416e+00
-		};
-		double q, r;
+    static const double c[] = {
+        -7.784894002430293e-03,
+        -3.223964580411365e-01,
+        -2.400758277161838e+00,
+        -2.549732539343734e+00,
+        4.374664141464968e+00,
+        2.938163982698783e+00
+    };
 
-		errno = 0;
+    static const double d[] = {
+        7.784695709041462e-03,
+        3.224671290700398e-01,
+        2.445134137142996e+00,
+        3.754408661907416e+00
+    };
+    double q, r;
 
-		if (clvl < 0 || clvl > 1)
-		{
-			errno = EDOM;
-			return 0.0;
-		}
-		else if (clvl == 0)
-		{
-			errno = ERANGE;
-			return -HUGE_VAL /* minus "infinity" */;
-		}
-		else if (clvl == 1)
-		{
-			errno = ERANGE;
-			return HUGE_VAL /* "infinity" */;
-		}
-		else if (clvl < 0.02425)
-		{
-			/* Rational approximation for lower region */
-			q = sqrt(-2 * log(clvl));
-			return (((((c[0] * q + c[1])*q + c[2])*q + c[3])*q + c[4])*q + c[5]) /
-				((((d[0] * q + d[1])*q + d[2])*q + d[3])*q + 1);
-		}
-		else if (clvl > 0.97575)
-		{
-			/* Rational approximation for upper region */
-			q = sqrt(-2 * log(1 - clvl));
-			return -(((((c[0] * q + c[1])*q + c[2])*q + c[3])*q + c[4])*q + c[5]) /
-				((((d[0] * q + d[1])*q + d[2])*q + d[3])*q + 1);
-		}
-		else
-		{
-			/* Rational approximation for central region */
-			q = clvl - 0.5;
-			r = q*q;
-			return (((((a[0] * r + a[1])*r + a[2])*r + a[3])*r + a[4])*r + a[5])*q /
-				(((((b[0] * r + b[1])*r + b[2])*r + b[3])*r + b[4])*r + 1);
-		}
-	}
+    errno = 0;
+
+    if (clvl < 0 || clvl > 1) {
+        errno = EDOM;
+        return 0.0;
+    } else if (clvl == 0) {
+        errno = ERANGE;
+        return -HUGE_VAL /* minus "infinity" */;
+    } else if (clvl == 1) {
+        errno = ERANGE;
+        return HUGE_VAL /* "infinity" */;
+    } else if (clvl < 0.02425) {
+        /* Rational approximation for lower region */
+        q = sqrt(-2 * log(clvl));
+        return (((((c[0] * q + c[1])*q + c[2])*q + c[3])*q + c[4])*q + c[5]) /
+               ((((d[0] * q + d[1])*q + d[2])*q + d[3])*q + 1);
+    } else if (clvl > 0.97575) {
+        /* Rational approximation for upper region */
+        q = sqrt(-2 * log(1 - clvl));
+        return -(((((c[0] * q + c[1])*q + c[2])*q + c[3])*q + c[4])*q + c[5]) /
+               ((((d[0] * q + d[1])*q + d[2])*q + d[3])*q + 1);
+    } else {
+        /* Rational approximation for central region */
+        q = clvl - 0.5;
+        r = q*q;
+        return (((((a[0] * r + a[1])*r + a[2])*r + a[3])*r + a[4])*r + a[5])*q /
+               (((((b[0] * r + b[1])*r + b[2])*r + b[3])*r + b[4])*r + 1);
+    }
+}
 }
 
 #endif /* __LUACL_MATH_HPP */
