3a2105ff5945c5babcbf47382a2f1569a8265692
Aean <v@aean.net>
2014-08-14 17:08:19 +0800

    Fix SetArg / SetArgMem. Code consistency.

--- src/luacl_buffer.hpp
+++ src/luacl_buffer.hpp
@@ -12,163 +12,163 @@ static const size_t LUACL_BUFFER_MIN_SIZE = 2;
 
 /* Buffer object, keeping cl_mem object and allocated memory. */
 struct luacl_buffer_object {
-	cl_mem mem = NULL;
-	void * data = NULL;
-	size_t size = 0;
+    cl_mem mem = NULL;
+    void * data = NULL;
+    size_t size = 0;
 
-	cl_int ReleaseMem() {
-		if (mem == NULL) {
-			return CL_SUCCESS;
-		}
-		cl_int err = clReleaseMemObject(mem);
-		mem = NULL;
-		return err;
-	}
+    cl_int ReleaseMem() {
+        if (mem == NULL) {
+            return CL_SUCCESS;
+        }
+        cl_int err = clReleaseMemObject(mem);
+        mem = NULL;
+        return err;
+    }
 
-	~luacl_buffer_object() {
-		ReleaseMem();
-		free(data);             /* According to C99, free(NULL) should always be safe. */
-		data = NULL;
-	}
+    ~luacl_buffer_object() {
+        ReleaseMem();
+        free(data);             /* According to C99, free(NULL) should always be safe. */
+        data = NULL;
+    }
 };
 
 typedef luacl_buffer_object* luacl_buffer_info;
 
 template <>
 struct luacl_object_constants<luacl_buffer_info> {
-	static const char * REGISTRY() {
-		return LUACL_MEM_REGISTRY;
-	}
-	static const char * METATABLE() {
-		return LUACL_MEM_METATABLE;
-	}
-	static const char * TOSTRING() {
-		return LUACL_MEM_TOSTRING;
-	}
-	static cl_int Release(luacl_buffer_info mem) {
-		cl_int err = mem->ReleaseMem();	/* The destructor will do this. In order to get err code, release it prior to destruction. */
-		delete mem;
-		return err;
-	}
+    static const char * REGISTRY() {
+        return LUACL_MEM_REGISTRY;
+    }
+    static const char * METATABLE() {
+        return LUACL_MEM_METATABLE;
+    }
+    static const char * TOSTRING() {
+        return LUACL_MEM_TOSTRING;
+    }
+    static cl_int Release(luacl_buffer_info mem) {
+        cl_int err = mem->ReleaseMem();	/* The destructor will do this. In order to get err code, release it prior to destruction. */
+        delete mem;
+        return err;
+    }
 };
 
 struct luacl_buffer {
-	typedef luacl_object<luacl_buffer_info> traits;
+    typedef luacl_object<luacl_buffer_info> traits;
 
-	static void Init(lua_State *L) {
-		luaL_newmetatable(L, LUACL_MEM_METATABLE);
-		lua_newtable(L);
+    static void Init(lua_State *L) {
+        luaL_newmetatable(L, LUACL_MEM_METATABLE);
+        lua_newtable(L);
         lua_pushcfunction(L, GetBufferSize);
         lua_setfield(L, -2, "GetBufferSize");
-		lua_pushcfunction(L, Clear);
+        lua_pushcfunction(L, Clear);
         lua_setfield(L, -2, "Clear");
-		RegisterType<int>(L, "Int");
+        RegisterType<int>(L, "Int");
         RegisterType<float>(L, "Float");
         RegisterType<double>(L, "Double");
         RegisterType<short>(L, "Short");
         RegisterType<char>(L, "Char");
-		lua_setfield(L, -2, "__index");
-		lua_pushcfunction(L, traits::ToString);
-		lua_setfield(L, -2, "__tostring");
-		lua_pushcfunction(L, traits::Release);
-		lua_setfield(L, -2, "__gc");
+        lua_setfield(L, -2, "__index");
+        lua_pushcfunction(L, traits::ToString);
+        lua_setfield(L, -2, "__tostring");
+        lua_pushcfunction(L, traits::Release);
+        lua_setfield(L, -2, "__gc");
 
-		traits::CreateRegistry(L);
-	}
+        traits::CreateRegistry(L);
+    }
 
-	static int Create(lua_State *L) {
-		cl_context context = luacl_object<cl_context>::CheckObject(L, 1);
-		size_t size = static_cast<size_t>(luaL_checknumber(L, 2));
-		cl_mem_flags flags = static_cast<cl_mem_flags>(lua_tonumber(L, 3));
-		flags = (flags == 0) ? (CL_MEM_READ_WRITE) : flags;
+    static int Create(lua_State *L) {
+        cl_context context = luacl_object<cl_context>::CheckObject(L, 1);
+        size_t size = static_cast<size_t>(luaL_checknumber(L, 2));
+        cl_mem_flags flags = static_cast<cl_mem_flags>(lua_tonumber(L, 3));
+        flags = (flags == 0) ? (CL_MEM_READ_WRITE) : flags;
 
-		if (size == 0) {
-			return luaL_error(L, "Bad argument #2: size of buffer must be greater than %d.", LUACL_BUFFER_MIN_SIZE);
-		}
-		void * data = malloc(size);
-		CheckAllocError(L, data);
-		memset(data, 0, size);
-		luacl_buffer_info bufferObject = new luacl_buffer_object;
-		bufferObject->data = data;		/* The allocated memory is now guarded by bufferObject */
-		bufferObject->size = size;
-		traits::Wrap(L, bufferObject);	/* Push the pointer of uncompleted buffer object to lua stack */
+        if (size == 0) {
+            return luaL_error(L, "Bad argument #2: size of buffer must be greater than %d.", LUACL_BUFFER_MIN_SIZE);
+        }
+        void * data = malloc(size);
+        CheckAllocError(L, data);
+        memset(data, 0, size);
+        luacl_buffer_info bufferObject = new luacl_buffer_object;
+        bufferObject->data = data;		/* The allocated memory is now guarded by bufferObject */
+        bufferObject->size = size;
+        traits::Wrap(L, bufferObject);	/* Push the pointer of uncompleted buffer object to lua stack */
 
-		cl_int err = 0;
-		cl_mem mem = clCreateBuffer(context, flags, size, NULL, &err);
-		CheckCLError(L, err, "Failed creating buffer: %d.");	/* Potential function exit, *data will be released by bufferObject destructor */
-		bufferObject->mem = mem;
-		return 1;
-	}
-    
+        cl_int err = 0;
+        cl_mem mem = clCreateBuffer(context, flags, size, NULL, &err);
+        CheckCLError(L, err, "Failed creating buffer: %d.");	/* Potential function exit, *data will be released by bufferObject destructor */
+        bufferObject->mem = mem;
+        return 1;
+    }
+
     static int GetBufferSize(lua_State *L) {
         luacl_buffer_info buffer = traits::CheckObject(L);
         lua_pushnumber(L, static_cast<lua_Number>(buffer->size));
         return 1;
     }
-    
+
     template <typename T>
     static void RegisterType(lua_State *L, const char * name) {
-		lua_pushcfunction(L, Get<T>);
-		lua_setfield(L, -2, std::string("Get").append(name).c_str());
-		lua_pushcfunction(L, Set<T>);
-		lua_setfield(L, -2, std::string("Set").append(name).c_str());
+        lua_pushcfunction(L, Get<T>);
+        lua_setfield(L, -2, std::string("Get").append(name).c_str());
+        lua_pushcfunction(L, Set<T>);
+        lua_setfield(L, -2, std::string("Set").append(name).c_str());
         lua_pushcfunction(L, GetSize<T>);
         lua_setfield(L, -2, std::string("GetSize").append(name).c_str());
         lua_pushcfunction(L, ReverseEndian<T>);
         lua_setfield(L, -2, std::string("ReverseEndian").append(name).c_str());
     }
 
-	template <typename T>
-	static int Get(lua_State *L) {
-		luacl_buffer_info buffer = traits::CheckObject(L, 1);
-		ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
-		
-		if (index < 0 || (index + 1) * sizeof(T) > buffer->size) {
-            luaL_error(L, "Buffer access out of bound.");
-        }
-		T * data = reinterpret_cast<T *>(buffer->data);
-		lua_pushnumber(L, static_cast<lua_Number>(data[index]));
-		return 1;
-	}
+    template <typename T>
+    static int Get(lua_State *L) {
+        luacl_buffer_info buffer = traits::CheckObject(L, 1);
+        ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
 
-	template <typename T>
-	static int Set(lua_State *L) {
-		luacl_buffer_info buffer = traits::CheckObject(L, 1);
-		ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
-		T value = static_cast<T>(lua_tonumber(L, 3));
-		
-		if (index < 0 || (index + 1) * sizeof(T) > buffer->size) {
+        if (index < 0 || (index + 1) * sizeof(T) > buffer->size) {
             luaL_error(L, "Buffer access out of bound.");
         }
-		T * data = reinterpret_cast<T *>(buffer->data);
-		data[index] = value;
-		//* (reinterpret_cast<T *>(buffer->data) + addr) = value; /* Compare de-asm for performance? */
-		return 0;
-	}
-    
+        T * data = reinterpret_cast<T *>(buffer->data);
+        lua_pushnumber(L, static_cast<lua_Number>(data[index]));
+        return 1;
+    }
+
+    template <typename T>
+    static int Set(lua_State *L) {
+        luacl_buffer_info buffer = traits::CheckObject(L, 1);
+        ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
+        T value = static_cast<T>(lua_tonumber(L, 3));
+
+        if (index < 0 || (index + 1) * sizeof(T) > buffer->size) {
+            luaL_error(L, "Buffer access out of bound.");
+        }
+        T * data = reinterpret_cast<T *>(buffer->data);
+        data[index] = value;
+        //* (reinterpret_cast<T *>(buffer->data) + addr) = value; /* Compare de-asm for performance? */
+        return 0;
+    }
+
     template <typename T>
     static int GetSize(lua_State *L) {
         lua_pushnumber(L, static_cast<lua_Number>(sizeof(T)));
         return 1;
     }
-    
+
     template <typename T>
     static int ReverseEndian(lua_State *L) {
-		luacl_buffer_info buffer = traits::CheckObject(L, 1);
-		ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
-		ptrdiff_t range = static_cast<ptrdiff_t>(lua_tonumber(L, 3));
+        luacl_buffer_info buffer = traits::CheckObject(L, 1);
+        ptrdiff_t index = static_cast<ptrdiff_t>(lua_tonumber(L, 2));
+        ptrdiff_t range = static_cast<ptrdiff_t>(lua_tonumber(L, 3));
         range = (range == 0) ? buffer->size / sizeof(T) - index : range;
 
         if (index < 0 || range < 0 || (index + 1) * sizeof(T) > buffer->size) {
             luaL_error(L, "Buffer access out of bound.");
         }
-		T * data = reinterpret_cast<T *>(buffer->data);
+        T * data = reinterpret_cast<T *>(buffer->data);
         LUACL_TRYCALL(
             util::luacl_byte_order_reverse<T>(data + index, range);
         );
-		return 0;
+        return 0;
     }
-    
+
     static int Clear(lua_State *L) {
         luacl_buffer_info buffer = traits::CheckObject(L);
         size_t offset = static_cast<size_t>(lua_tonumber(L, 2));
--- src/luacl_cmdqueue.hpp
+++ src/luacl_cmdqueue.hpp
@@ -12,159 +12,160 @@ static const char LUACL_CMDQUEUE_TOSTRING[] = "LuaCL_CmdQueue";
 
 template <>
 struct luacl_object_constants<cl_command_queue> {
-	static const char * REGISTRY() {
-		return LUACL_CMDQUEUE_REGISTRY;
-	}
-	static const char * METATABLE() {
-		return LUACL_CMDQUEUE_METATABLE;
-	}
-	static const char * TOSTRING() {
-		return LUACL_CMDQUEUE_TOSTRING;
-	}
-	static cl_int Release(cl_command_queue cmdqueue) {
-		return clReleaseCommandQueue(cmdqueue);
-	}
+    static const char * REGISTRY() {
+        return LUACL_CMDQUEUE_REGISTRY;
+    }
+    static const char * METATABLE() {
+        return LUACL_CMDQUEUE_METATABLE;
+    }
+    static const char * TOSTRING() {
+        return LUACL_CMDQUEUE_TOSTRING;
+    }
+    static cl_int Release(cl_command_queue cmdqueue) {
+        return clReleaseCommandQueue(cmdqueue);
+    }
 };
 
 struct luacl_cmdqueue {
     typedef luacl_object<cl_command_queue> traits;
 
-	static void Init(lua_State *L) {
-		luaL_newmetatable(L, LUACL_CMDQUEUE_METATABLE);
-		lua_newtable(L);
-		lua_pushcfunction(L, EnqueueWriteBuffer);
-		lua_setfield(L, -2, "EnqueueWriteBuffer");
-		lua_pushcfunction(L, EnqueueReadBuffer);
-		lua_setfield(L, -2, "EnqueueReadBuffer");
-		lua_pushcfunction(L, EnqueueNDRangeKernel);
-		lua_setfield(L, -2, "EnqueueNDRangeKernel");
-		lua_setfield(L, -2, "__index");
-		lua_pushcfunction(L, traits::ToString);
-		lua_setfield(L, -2, "__tostring");
-		lua_pushcfunction(L, traits::Release);
-		lua_setfield(L, -2, "__gc");
-
-		traits::CreateRegistry(L);
-	}
-
-    static int Create(lua_State *L) {
-		cl_context context = luacl_object<cl_context>::CheckObject(L);
-		cl_device_id device = luacl_object<cl_device_id>::CheckObject(L, 2);
-		lua_checkstack(L, 4);
-		int outOfOrder = lua_toboolean(L, 3);
-		int profiling = lua_toboolean(L, 4);
-		cl_command_queue_properties prop = outOfOrder ? CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE : 0x0;
-		prop |= profiling ? CL_QUEUE_PROFILING_ENABLE : 0x0;
-
-		cl_int err = 0;
-		cl_command_queue cmdqueue = clCreateCommandQueue(context, device, prop, &err);
-		CheckCLError(L, err, "Failed creating command queue: %d.");
-		traits::Wrap(L, cmdqueue);
-    	return 1;
+    static void Init(lua_State *L) {
+        luaL_newmetatable(L, LUACL_CMDQUEUE_METATABLE);
+        lua_newtable(L);
+        lua_pushcfunction(L, Finish);
+        lua_setfield(L, -2, "Finish");
+        lua_pushcfunction(L, EnqueueWriteBuffer);
+        lua_setfield(L, -2, "EnqueueWriteBuffer");
+        lua_pushcfunction(L, EnqueueReadBuffer);
+        lua_setfield(L, -2, "EnqueueReadBuffer");
+        lua_pushcfunction(L, EnqueueNDRangeKernel);
+        lua_setfield(L, -2, "EnqueueNDRangeKernel");
+        lua_setfield(L, -2, "__index");
+        lua_pushcfunction(L, traits::ToString);
+        lua_setfield(L, -2, "__tostring");
+        lua_pushcfunction(L, traits::Release);
+        lua_setfield(L, -2, "__gc");
+        traits::CreateRegistry(L);
     }
 
-	static int EnqueueNDRangeKernel(lua_State *L) {
-		cl_command_queue cmdqueue = traits::CheckObject(L);
-		cl_kernel krnl = luacl_object<cl_kernel>::CheckObject(L, 2);
-		std::vector<size_t> localWorkSize = luacl_object<size_t>::CheckNumberTable(L, 3, true);	/* Optional arg */
-		std::vector<size_t> globalWorkSize = luacl_object<size_t>::CheckNumberTable(L, 4);
-		std::vector<size_t> globalWorkOffset = luacl_object<size_t>::CheckNumberTable(L, 5, true);	/* Optional arg */
+    static int Create(lua_State *L) {
+        cl_context context = luacl_object<cl_context>::CheckObject(L);
+        cl_device_id device = luacl_object<cl_device_id>::CheckObject(L, 2);
+        lua_checkstack(L, 4);
+        int outOfOrder = lua_toboolean(L, 3);
+        int profiling = lua_toboolean(L, 4);
+        cl_command_queue_properties prop = outOfOrder ? CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE : 0x0;
+        prop |= profiling ? CL_QUEUE_PROFILING_ENABLE : 0x0;
 
-		/* Work dimension extraction and check */
-		cl_uint workDim = static_cast<cl_uint>(globalWorkSize.size());
-		if (workDim == 0) {
-			return luaL_error(L, "Invalid working dimension.");
-		}
-		/* Dimension of local work size must match. Global work offset could match the dimension or be (optionally) empty */
-		if ((!localWorkSize.empty() && workDim != localWorkSize.size()) || 
-			(!globalWorkOffset.empty() && globalWorkOffset.size() != workDim)) {
-			return luaL_error(L, "Working dimension mismatch.");
-		}
+        cl_int err = 0;
+        cl_command_queue cmdqueue = clCreateCommandQueue(context, device, prop, &err);
+        CheckCLError(L, err, "Failed creating command queue: %d.");
+        traits::Wrap(L, cmdqueue);
+        return 1;
+    }
 
-		std::vector<cl_event> events = luacl_object<cl_event>::CheckObjectTable(L, 6, true);	/* Event list could be empty */
+    static int EnqueueNDRangeKernel(lua_State *L) {
+        cl_command_queue cmdqueue = traits::CheckObject(L);
+        cl_kernel krnl = luacl_object<cl_kernel>::CheckObject(L, 2);
+        std::vector<size_t> localWorkSize = luacl_object<size_t>::CheckNumberTable(L, 3, true);	/* Optional arg */
+        std::vector<size_t> globalWorkSize = luacl_object<size_t>::CheckNumberTable(L, 4);
+        std::vector<size_t> globalWorkOffset = luacl_object<size_t>::CheckNumberTable(L, 5, true);	/* Optional arg */
 
-		const cl_event * eventsData = events.empty() ? NULL : events.data();
-		const cl_uint eventSize = static_cast<cl_uint>(events.size());
+        /* Work dimension extraction and check */
+        cl_uint workDim = static_cast<cl_uint>(globalWorkSize.size());
+        if (workDim == 0) {
+            return luaL_error(L, "Invalid working dimension.");
+        }
+        /* Dimension of local work size must match. Global work offset could match the dimension or be (optionally) empty */
+        if ((!localWorkSize.empty() && workDim != localWorkSize.size()) ||
+                (!globalWorkOffset.empty() && globalWorkOffset.size() != workDim)) {
+            return luaL_error(L, "Working dimension mismatch.");
+        }
 
-		cl_event event = NULL;
-		cl_int err = clEnqueueNDRangeKernel(
-            cmdqueue,
-            krnl,
-            workDim,
-			globalWorkOffset.empty() ? NULL : globalWorkOffset.data(),
-            globalWorkSize.data(),
-            localWorkSize.empty() ? NULL : localWorkSize.data(),
-            eventSize,
-            eventsData,
-            &event
-        );
-		CheckCLError(L, err, "Failed requesting enqueue NDRange: %d.");
+        std::vector<cl_event> events = luacl_object<cl_event>::CheckObjectTable(L, 6, true);	/* Event list could be empty */
+
+        const cl_event * eventsData = events.empty() ? NULL : events.data();
+        const cl_uint eventSize = static_cast<cl_uint>(events.size());
+
+        cl_event event = NULL;
+        cl_int err = clEnqueueNDRangeKernel(
+                         cmdqueue,
+                         krnl,
+                         workDim,
+                         globalWorkOffset.empty() ? NULL : globalWorkOffset.data(),
+                         globalWorkSize.data(),
+                         localWorkSize.empty() ? NULL : localWorkSize.data(),
+                         eventSize,
+                         eventsData,
+                         &event
+                     );
+        CheckCLError(L, err, "Failed requesting enqueue NDRange: %d.");
         return 0;
-	}
+    }
 
-	static int EnqueueWriteBuffer(lua_State *L) {
-		cl_command_queue cmdqueue = traits::CheckObject(L);
-		luacl_buffer_info buffer = luacl_object<luacl_buffer_info>::CheckObject(L, 2);
-		std::vector<cl_event> eventList = luacl_object<cl_event>::CheckObjectTable(L, 3, true);
-		size_t size = static_cast<size_t>(lua_tonumber(L, 4));
-		size_t offset = static_cast<size_t>(lua_tonumber(L, 5));
-		size = (size == 0) ? buffer->size : size;
-		if (size + offset > buffer->size) {
-			return luaL_error(L, "Invalid size or offset.");
-		}
-		cl_bool blocking = lua_toboolean(L, 6);
-		cl_event event = NULL;
-		cl_int err = clEnqueueWriteBuffer(
-            cmdqueue,
-            buffer->mem,
-            blocking,
-            offset,
-            size,
-            buffer->data,
-            static_cast<cl_uint>(eventList.size()),
-            eventList.empty() ? NULL : eventList.data(),
-            &event
-        );
-		CheckCLError(L, err, "Failed requesting enqueue write buffer: %d.");
-		luacl_object<cl_event>::Wrap(L, event);
-		return 1;
-	}
+    static int EnqueueWriteBuffer(lua_State *L) {
+        cl_command_queue cmdqueue = traits::CheckObject(L);
+        luacl_buffer_info buffer = luacl_object<luacl_buffer_info>::CheckObject(L, 2);
+        std::vector<cl_event> eventList = luacl_object<cl_event>::CheckObjectTable(L, 3, true);
+        size_t size = static_cast<size_t>(lua_tonumber(L, 4));
+        size_t offset = static_cast<size_t>(lua_tonumber(L, 5));
+        size = (size == 0) ? buffer->size : size;
+        if (size + offset > buffer->size) {
+            return luaL_error(L, "Invalid size or offset.");
+        }
+        cl_bool blocking = lua_toboolean(L, 6);
+        cl_event event = NULL;
+        cl_int err = clEnqueueWriteBuffer(
+                         cmdqueue,
+                         buffer->mem,
+                         blocking,
+                         offset,
+                         size,
+                         buffer->data,
+                         static_cast<cl_uint>(eventList.size()),
+                         eventList.empty() ? NULL : eventList.data(),
+                         &event
+                     );
+        CheckCLError(L, err, "Failed requesting enqueue write buffer: %d.");
+        luacl_object<cl_event>::Wrap(L, event);
+        return 1;
+    }
 
-	static int EnqueueReadBuffer(lua_State *L) {
-		cl_command_queue cmdqueue = traits::CheckObject(L);
-		luacl_buffer_info buffer = luacl_object<luacl_buffer_info>::CheckObject(L, 2);
-		std::vector<cl_event> eventList = luacl_object<cl_event>::CheckObjectTable(L, 3);
-		size_t size = static_cast<size_t>(lua_tonumber(L, 4));
-		size_t offset = static_cast<size_t>(lua_tonumber(L, 5));
-		size = size == 0 ? buffer->size : size;
-		if (size + offset > buffer->size) {
-			return luaL_error(L, "Invalid size or offset.");
-		}
-		cl_bool blocking = lua_toboolean(L, 6);
-        
-		cl_event event = NULL;
-		cl_int err = clEnqueueReadBuffer(
-            cmdqueue,
-            buffer->mem,
-            blocking,
-            offset,
-            size,
-            buffer->data,
-            static_cast<cl_uint>(eventList.size()),
-            eventList.empty() ? NULL : eventList.data(),
-            &event
-        );
-		CheckCLError(L, err, "Failed requesting enqueue read buffer: %d.");
-		luacl_object<cl_event>::Wrap(L, event);
-		return 1;
-	}
+    static int EnqueueReadBuffer(lua_State *L) {
+        cl_command_queue cmdqueue = traits::CheckObject(L);
+        luacl_buffer_info buffer = luacl_object<luacl_buffer_info>::CheckObject(L, 2);
+        std::vector<cl_event> eventList = luacl_object<cl_event>::CheckObjectTable(L, 3);
+        size_t size = static_cast<size_t>(lua_tonumber(L, 4));
+        size_t offset = static_cast<size_t>(lua_tonumber(L, 5));
+        size = size == 0 ? buffer->size : size;
+        if (size + offset > buffer->size) {
+            return luaL_error(L, "Invalid size or offset.");
+        }
+        cl_bool blocking = lua_toboolean(L, 6);
 
-	static int Finish(lua_State *L) {
-		cl_command_queue cmdqueue = traits::CheckObject(L);
-		cl_int err = clFinish(cmdqueue);
-		CheckCLError(L, err, "Failed finishing command queue: %d.");
-		return 0;
-	}
+        cl_event event = NULL;
+        cl_int err = clEnqueueReadBuffer(
+                         cmdqueue,
+                         buffer->mem,
+                         blocking,
+                         offset,
+                         size,
+                         buffer->data,
+                         static_cast<cl_uint>(eventList.size()),
+                         eventList.empty() ? NULL : eventList.data(),
+                         &event
+                     );
+        CheckCLError(L, err, "Failed requesting enqueue read buffer: %d.");
+        luacl_object<cl_event>::Wrap(L, event);
+        return 1;
+    }
+
+    static int Finish(lua_State *L) {
+        cl_command_queue cmdqueue = traits::CheckObject(L);
+        cl_int err = clFinish(cmdqueue);
+        CheckCLError(L, err, "Failed finishing command queue: %d.");
+        return 0;
+    }
 };
 
 #endif /* __LUACL_CMDQUEUE_HPP */
