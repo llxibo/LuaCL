b1694176a7a669bf2765e877fb16fe1ea4256139
llxibo <llxibo@126.com>
2014-08-16 21:39:59 +0800

    Fix LUACL_UNLIKELY for non-GNU compilers

--- src/LuaCL.h
+++ src/LuaCL.h
@@ -4,51 +4,57 @@
 /* Include Lua libraries */
 /* Lua.h is not protected with extern C */
 extern "C" {
-#pragma warning(push)
-#pragma warning(disable:4334)
-    #include <stdio.h>
-    #include <string.h>
-    #include "lua.h"
-    #include "lauxlib.h"
-    #include "lualib.h"
-#pragma warning(pop)
+#   pragma warning(push)
+#   pragma warning(disable:4334)
+#   include <stdio.h>
+#   include <string.h>
+#   include "lua.h"
+#   include "lauxlib.h"
+#   include "lualib.h"
+#   pragma warning(pop)
 } /* extern "C" */
 
 /* Include OpenCL libraries */
 /* OpenCL.h has built-in extern C protection */
 #ifdef __APPLE__
-#include <OpenCL/cl.h>
+#   include <OpenCL/cl.h>
 #else
-#include "CL/OpenCL.h"
+#   include "CL/OpenCL.h"
 #endif
 
 /* Switch for pausing the command line when debugging in MSVC */
 #if defined(_MSC_VER)// && defined(_DEBUG)
-#define _LUACL_PAUSE_SYSTEM
+#   define _LUACL_PAUSE_SYSTEM
 #endif
 
 #if defined(_LUACL_PAUSE_SYSTEM)
-#include <stdlib.h>
+#   include <stdlib.h>
 #endif
 
 /* Structured Exception Handling (SEH) of Windows could handle potential system exceptions
    in OpenCL calls as C++ exception, and therefore recover from it. */
 #ifdef _MSC_VER
-#define LUACL_TRYCALL(call) __try { \
-        call                        \
-    }                               \
-    __except (1) {                  \
+#   define LUACL_TRYCALL(call) __try {                                      \
+        call                                                                \
+    }                                                                       \
+    __except (1) {                                                          \
         luaL_error(L, "Fatal error: error executing OpenCL API %s", #call); \
     }
 #else
-#define LUACL_TRYCALL(call) {call;}
+#   define LUACL_TRYCALL(call) {call;}
 #endif
 
 //#define LUACL_TRYCALL(call) do { _LUACL_TRYCALL(call); } while (0)
 
-#define LUACL_UNUSED(expr)      (void *)expr;
-#define LUACL_LIKELY(expr)      __builtin_expect(!!(expr), true)
-#define LUACL_UNLIKELY(expr)    __builtin_expect(!!(expr), false)
+#if defined(__GNUC__)
+#   define LUACL_LIKELY(expr)   __builtin_expect(!!(expr), true)
+#   define LUACL_UNLIKELY(expr) __builtin_expect(!!(expr), false)
+#else
+#   define LUACL_LIKELY(expr)   (expr)
+#   define LUACL_UNLIKELY(expr) (expr)
+#endif
+
+#define LUACL_UNUSED(expr)      (void)expr;
 
 /* Include LuaCL modules */
 #include "luacl_debug.hpp"
