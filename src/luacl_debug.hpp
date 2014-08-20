df67a479b561b345a0d2dbce96c1c1181c0489d5
llxibo <llxibo@126.com>
2014-08-20 13:20:27 +0800

    Updated luacl_debug with traits::RegisterFunction

--- src/luacl_debug.hpp
+++ src/luacl_debug.hpp
@@ -2,21 +2,23 @@
 #define __LUACL_DEBUG_HPP
 
 #include "LuaCL.h"
+#include "luacl_object.hpp"
 
 const char LUACL_DEBUG_CALLBACK_FUNC[] = "LuaCL_Debug_Callback_Func";
 
 #if defined(_DEBUG)
+    /* Debug function. Calls registered callback function with debug string. [-0, +0, e] */
     inline void l_debug(lua_State *L, const char * message, ...) {
-        lua_getfield(L, LUA_REGISTRYINDEX, LUACL_DEBUG_CALLBACK_FUNC);
+        lua_getfield(L, LUA_REGISTRYINDEX, LUACL_DEBUG_CALLBACK_FUNC);  /* func/nil */
         if (!lua_isfunction(L, -1)) {
-            lua_pop(L, 1);
+            lua_pop(L, 1);      /* Pop nil to keep stack balanced */
             return;
         }
         va_list argp;
         va_start(argp, message);
-        lua_pushvfstring(L, message, argp);
+        lua_pushvfstring(L, message, argp);                             /* str, func */
         va_end(argp);
-        lua_call(L, 1, 0);
+        lua_call(L, 1, 0);                                              /* (empty stack) */
     }
 #else
     void l_debug(lua_State *L, const char * message, ...) {
@@ -26,16 +28,16 @@ const char LUACL_DEBUG_CALLBACK_FUNC[] = "LuaCL_Debug_Callback_Func";
 #endif /* _DEBUG */
 
 struct luacl_debug {
+    typedef luacl_object<void *> traits;
+
     static int Init(lua_State *L) {
-        lua_pushcfunction(L, RegisterDebugCallback);
-        lua_setfield(L, LUA_GLOBALSINDEX, "RegisterDebugCallback");
-        lua_pushcfunction(L, GetRegistry);
-        lua_setfield(L, LUA_GLOBALSINDEX, "GetRegistry");
-        lua_pushcfunction(L, GetOpenCLVersion);
-        lua_setfield(L, LUA_GLOBALSINDEX, "GetOpenCLVersion");
+        traits::RegisterFunction(L, RegisterDebugCallback, "RegisterDebugCallback", LUA_GLOBALSINDEX);
+        traits::RegisterFunction(L, GetRegistry, "GetRegistry", LUA_GLOBALSINDEX);
+        traits::RegisterFunction(L, GetOpenCLVersion, "GetOpenCLVersion", LUA_GLOBALSINDEX);
         return 0;
     }
 
+    /* Lua function. Registers a function as debug callback function. [-0, +0, m] */
     static int RegisterDebugCallback(lua_State *L) {
         lua_pushvalue(L, 1);
         lua_setfield(L, LUA_REGISTRYINDEX, LUACL_DEBUG_CALLBACK_FUNC);
